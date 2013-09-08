adc915c216549b9ff804d9c6870811fcd1e7a8b0
Clay Stewart <claytonmstewart@gmail.com>
2013-09-07 23:41:36 -0500

    Add requirements from Exercise 37

--- car.rb
+++ car.rb
@@ -9,18 +9,18 @@
 
 
 class Car
-	def initialize
-		@fuel = 10
-		@distance = 0
-		puts "the initialize method is running automatically"
-	end 
+  def initialize
+    @fuel = 10
+    @distance = 0
+    puts "the initialize method is running automatically"
+  end 
 
-	def get_info
-		"I'm a car. I've driven #{@distance} miles and have #{@fuel} gallons of gas left."
-	end
+  def get_info
+    "I'm a car. I've driven #{@distance} miles and have #{@fuel} gallons of gas left."
+  end
 
-	def drive(miles)
-		@fuel = @fuel - (20.0 * miles)
-		@distance = @distance + miles
-	end
+  def drive(miles)
+    @fuel = @fuel - (miles/20.0)
+    @distance = @distance + miles
+  end
 end
---
+++ car_main.rb
@@ -0,0 +1,6 @@
+require_relative 'car'
+
+old_blue = Car.new()
+old_blue.get_info
+old_blue.drive(10)
+old_blue.get_info
